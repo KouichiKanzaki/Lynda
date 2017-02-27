83da091fcc52ea9f183972ff1866321a8ac7de14
kouichi <likfsm4@gmail.com>
2017-02-27 21:57:44 +0900

    no message

--- Javascript講座/typescript/typescript_test.js
+++ Javascript講座/typescript/typescript_test.js
@@ -9,46 +9,116 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var Point = (function () {
+    //static RAD_TO_DEG: number = 180 / Math.PI;
     function Point(x, y) {
+        if (x === void 0) { x = 0; }
+        if (y === void 0) { y = 0; }
         this.x = x;
         this.y = y;
     }
-    Point.prototype.getLength = function () {
-        var square = this.x * this.x + this.y * this.y;
-        return Math.sqrt(square);
-    };
-    Point.prototype.getAngle = function () {
-        return Math.atan2(this.y, this.x);
-    };
+    Object.defineProperty(Point.prototype, "length", {
+        get: function () {
+            var square = this.x * this.x + this.y * this.y;
+            return Math.sqrt(square);
+        },
+        set: function (length) {
+            var point = Point.getPolar(length, this.angle);
+            this.x = point.x;
+            this.y = point.y;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Point.prototype, "angle", {
+        get: function () {
+            return Math.atan2(this.y, this.x);
+        },
+        set: function (angle) {
+            var point = Point.getPolar(this.length, angle);
+            this.x = point.x;
+            this.y = point.y;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    ;
     Point.polar = function (length, angle) {
+        var point = Point.getPolar(length, angle);
+        return new Point(point.x, point.y);
+    };
+    Point.getPolar = function (length, angle) {
         var x = length * Math.cos(angle);
         var y = length * Math.sin(angle);
-        return new Point(x, y);
+        return { x: x, y: y };
     };
+    Object.defineProperty(Point, "RAD_TO_DEG", {
+        get: function () {
+            return 180 / Math.PI;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return Point;
 }());
-Point.RAD_TO_DEG = 180 / Math.PI;
 var Vector = (function (_super) {
     __extends(Vector, _super);
     function Vector(x, y) {
+        if (x === void 0) { x = 0; }
+        if (y === void 0) { y = 0; }
         return _super.call(this, x, y) || this;
     }
-    Vector.prototype.scale = function (scale) {
-        this.x *= scale;
-        this.y *= scale;
+    Vector.prototype.scale = function (scaleX, scaleY) {
+        this.x *= scaleX;
+        this.y *= isNaN(scaleY) ? scaleX : scaleY;
+        ;
     };
     Vector.prototype.add = function (point) {
         this.x += point.x;
         this.y += point.y;
     };
     Vector.polar = function (length, angle) {
-        var point = Point.polar(length, angle);
+        var point = Point.getPolar(length, angle);
         return new Vector(point.x, point.y);
     };
     return Vector;
 }(Point));
-var obj = Vector.polar(1, 1);
-obj.scale(2);
-console.log(obj.x, obj.y);
-console.log(obj.getLength(), obj.getAngle() * Point.RAD_TO_DEG);
+var matrix = [
+    [1, 2, 3],
+    [1, 2, 3]
+];
+function sumMatrix(matrix) {
+    var sum = 0;
+    var j = 100;
+    for (var i = 0; i < matrix.length; i++) {
+        var currentRow = matrix[i];
+        var j_1 = 200;
+        for (var i_1 = 0; i_1 < currentRow.length; i_1++) {
+            var j_2 = 300;
+            sum += currentRow[i_1];
+            console.log(i_1, currentRow[i_1], j_2);
+        }
+        console.log(j_1);
+    }
+    console.log(j);
+    return sum;
+}
+console.log(sumMatrix(matrix));
+/*
+for(let i:number = 0;i<5;i++){
+        setTimeout(function(): void{
+            console.log(i);
+        },100*i);
+}
+console.log("i = " +i);
+*/
+//const RAD_TO_DEG: number = 180 / Math.PI;
+//var obj: Vector = new Vector(1, Math.sqrt(3));
+//console.log(obj.length, obj.angle * Point.RAD_TO_DEG);
+//console.log(Point.getPolar(Math.SQRT2, Math.PI / 4));
+/*
+var obj: Vector = Vector.polar(0, 0);
+obj.length = 1;
+obj.angle = Math.PI / 3;
+
+*/ 
 //# sourceMappingURL=typescript_test.js.map
--- Javascript講座/typescript/typescript_test.ts
+++ Javascript講座/typescript/typescript_test.ts
@@ -1,38 +1,106 @@
-class Point{
-    static RAD_TO_DEG: number = 180 / Math.PI;
-    constructor(public x: number, public y: number){
+class Point implements IPoint{
+    //static RAD_TO_DEG: number = 180 / Math.PI;
+    constructor(public x: number = 0, public y: number = 0){
     }
-    getLength(): number{
+    /*
+    set length(length: number){
+        var point: IPoint = Point.getPolar(length, this.angle);
+        this.x = point.x;
+        this.y = point.y;
+    }
+    get length(): number{
         var square :number = this.x * this.x + this.y * this.y;
         return Math.sqrt(square);
     }
-    getAngle(): number{
+    set angle(angle: number){
+        var point: IPoint = Point.getPolar(this.length, angle);
+        this.x = point.x;
+        this.y = point.y;
+    };
+    get angle(): number{
         return Math.atan2(this.y, this.x);
     }
     static polar(length: number, angle: number): Point{
+        var point: IPoint = Point.getPolar(length, angle);
+        return new Point(point.x, point.y);
+    }
+    protected static getPolar(length: number, angle: number): IPoint{
         var x: number = length * Math.cos(angle);
         var y: number = length * Math.sin(angle);
-        return new Point(x, y);
+        return {x: x, y: y}
     }
+    static get RAD_TO_DEG(): number {
+        return 180 / Math.PI; 
+    }
+    */
 }
 class Vector extends Point{
-    constructor(x: number, y: number){
+    constructor(x: number = 0, y: number = 0){
         super(x, y);
     }
-    scale(scale: number): void{
-        this.x *= scale;
-        this.y *= scale;
+    scale(scaleX: number, scaleY?: number): void{
+        this.x *= scaleX;
+        this.y *= isNaN(scaleY) ? scaleX : scaleY;;
     }
-    add(point: Point): void{
+    add(point: IPoint): void{
         this.x += point.x;
         this.y += point.y;
     }
     static polar(length: number, angle: number): Vector{
-        var point: Point = Point.polar(length, angle)
+        var point: IPoint = Point.getPolar(length, angle);
         return new Vector(point.x, point.y);
     }
 }
-var obj: Vector = Vector.polar(1, 1);
-obj.scale(2);
-console.log(obj.x,obj.y);
-console.log(obj.getLength(), obj.getAngle() * Point.RAD_TO_DEG);
+class Point3D {
+    constructor(public x: number = 0, public y: number = 0, public z: number = 0){
+    }
+}
+interface IPoint{
+    x: number;
+    y: number;
+}
+let value = new Point();
+value = new Vector();
+value = {x:0,y:1};
+value = new Point3D(0,0,0);
+/*
+let matrix: any[][] = [
+    [1,2,3],
+    [1,2,3]
+];
+function sumMatrix(matrix: number[][]): number{
+    let sum:number = 0;
+    const j: number = 100;
+    for(let i: number = 0;i<matrix.length;i++){
+         let currentRow: number[] = matrix[i];
+         const j: number = 200;
+        for(let i: number = 0;i<currentRow.length;i++){
+            const j: number = 300;
+            sum += currentRow[i];
+            console.log(i, currentRow[i], j);
+        }
+        console.log(j);
+    }
+    console.log(j);
+    return sum;
+}
+console.log(sumMatrix(matrix));
+*/
+/*
+for(let i:number = 0;i<5;i++){
+        setTimeout(function(): void{
+            console.log(i);
+        },100*i);
+}
+console.log("i = " +i);
+*/
+//const RAD_TO_DEG: number = 180 / Math.PI;
+var obj: Point = new Point3D(1, 2, 3);
+//console.log(obj.length, obj.angle * Point.RAD_TO_DEG);
+//console.log(Point.getPolar(Math.SQRT2, Math.PI / 4));
+/*
+var obj: Vector = Vector.polar(0, 0);
+obj.length = 1;
+obj.angle = Math.PI / 3;
+
+*/
